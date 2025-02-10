3e4e003c4bc5668240c87b938a17afb26bf4c866
Marlon-S <marlonsuyuc@gmail.com>
2025-02-10 17:56:25 -0600

    feat: add mock data for payment methods, organizations, and identity document types

---
+++ src/mocks/index.ts
@@ -0,0 +1,100 @@
+export const formaPagos = [
+  {
+    id: 1,
+    name: "Pago de contado",
+  },
+  {
+    id: 2,
+    name: "Pago a crédito",
+  },
+]
+
+export const metodoPagos = [
+  {
+    id: 10,
+    name: "Efectivo",
+  },
+  {
+    id: 42,
+    name: "Consignación",
+  },
+  {
+    id: 20,
+    name: "Cheque",
+  },
+  {
+    id: 47,
+    name: "Transferencia",
+  },
+  {
+    id: 71,
+    name: "Bonos",
+  },
+  {
+    id: 72,
+    name: "Vales",
+  },
+  {
+    id: 1,
+    name: "Medio de pago no definido",
+  },
+  {
+    id: 49,
+    name: "Tarjeta Débito",
+  },
+  {
+    id: 48,
+    name: "Tarjeta Crédito",
+  },
+  {
+    id: "ZZZ",
+    name: "Otro",
+  },
+]
+
+export const tiposOrganizaciones = [
+  {
+    id: 1,
+    name: "Persona Jurídica",
+  },
+  {
+    id: 2,
+    name: "Persona Natural",
+  },
+]
+
+export const cutomerTributos = [
+  {
+    id: 18,
+    name: "IVA",
+  },
+  {
+    id: 21,
+    name: "No aplica",
+  },
+]
+
+export const IVAExcluida = [
+  {
+    id: 0,
+    name: "NO",
+  },
+  {
+    id: 1,
+    name: "SI",
+  },
+]
+
+export const tiposDocumentosIdentidad = [
+  { id: 1, name: "Registro civil" },
+  { id: 2, name: "Tarjeta de identidad" },
+  { id: 3, name: "Cédula de ciudadanía" },
+  { id: 4, name: "Tarjeta de extranjería" },
+  { id: 5, name: "Cédula de extranjería" },
+  { id: 6, name: "NIT" },
+  { id: 7, name: "Pasaporte" },
+  { id: 8, name: "Documento de identificación extranjero" },
+  { id: 9, name: "PEP" },
+  { id: 10, name: "NIT otro país" },
+  { id: 11, name: "NUIP*" },
+]
